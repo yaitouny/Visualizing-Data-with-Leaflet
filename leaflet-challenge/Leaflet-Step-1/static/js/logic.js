c75c289723db74f5256e5c9670140f830ce1431f
Yasmine Aitouny <aitouny680@gmail.com>
2020-08-03 16:51:24 -0500

    fix circles opacity

--- leaflet-challenge/Leaflet-Step-1/static/js/logic.js
+++ leaflet-challenge/Leaflet-Step-1/static/js/logic.js
@@ -58,12 +58,16 @@ function makeMap() {
   var fullURL =
     "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
+  var tectonicPlatesURL =
+    "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json";
+
   d3.json(fullURL).then(function (response) {
-    // console.log(data);
+    console.log(response);
 
     //create markers and heatmap
     var markers = L.markerClusterGroup(); //this is already a master layer
     var heatArray = [];
+    var circles = [];
 
     var earthquakes = response.features;
 
@@ -77,7 +81,7 @@ function makeMap() {
           +earthquake.geometry.coordinates[1],
           +earthquake.geometry.coordinates[0],
         ]).bindPopup(
-          `<h6>${earthquake.properties.place}</h6><hr><h6>Magnitude: ${earthquake.properties.mag}</h6>`
+          `<h4>${earthquake.properties.place}</h4><hr><p>Magnitude: ${earthquake.properties.mag}</p>`
         );
         markers.addLayer(temp);
 
@@ -86,38 +90,118 @@ function makeMap() {
           +earthquake.geometry.coordinates[1],
           +earthquake.geometry.coordinates[0],
         ]);
+
+        var circle = L.circle(
+          [
+            +earthquake.geometry.coordinates[1],
+            +earthquake.geometry.coordinates[0],
+          ],
+          {
+            fillOpacity: 0.5,
+            color: "red",
+            fillColor: circleColor(earthquake.properties.mag),
+            radius: markerSize(earthquake.properties.mag),
+            stroke: true,
+            weight: 1,
+          }
+        ).bindPopup(
+          `<h4>${earthquake.properties.place}</h4><hr><p>Magnitude: ${earthquake.properties.mag}</p>`
+        );
+        circles.push(circle);
       }
     });
 
-    //create heatmap layer
-    var heat = L.heatLayer(heatArray, {
-      radius: 60,
-      blur: 40,
+    d3.json(tectonicPlatesURL).then(function (plates) {
+      let plateLayer = L.geoJson(plates, {
+        style: function (feature) {
+          return {
+            color: "orange",
+            weight: 1.5,
+          };
+        },
+      });
+      //create heatmap layer
+      var heat = L.heatLayer(heatArray, {
+        radius: 60,
+        blur: 40,
+      });
+
+      // create circle layer
+      var circleLayer = L.layerGroup(circles);
+
+      // Create a baseMaps object to contain the streetmap and darkmap
+      var baseMaps = {
+        Street: streetmap,
+        Dark: darkmap,
+        Light: lightmap,
+        Satellite: satellitemap,
+      };
+
+      // Create an overlayMaps object here to contain the "State Population" and "City Population" layers
+      var overlayMaps = {
+        Heatmap: heat,
+        Markers: markers,
+        Circles: circleLayer,
+        "Tectonic plates": plateLayer,
+      };
+
+      // Creating map object
+      var myMap = L.map("map", {
+        center: [40.73, -74.0059],
+        zoom: 3,
+        layers: [darkmap, markers, plateLayer],
+      });
+
+      // Create a layer control, containing our baseMaps and overlayMaps, and add them to the map
+      myMap.addLayer(markers);
+      L.control.layers(baseMaps, overlayMaps).addTo(myMap);
+
+      // Create a legend
+      var legend = L.control({ position: "bottomleft" });
+      legend.onAdd = function () {
+        var div = L.DomUtil.create("div", "info legend");
+
+        //create HTML for legend (has to be i tags)
+        div.innerHTML += "<h4>Magnitudes</h4>";
+        div.innerHTML +=
+          '<i style="background: #FFC300"></i><span>0-1</span><br>';
+        div.innerHTML +=
+          '<i style="background: #FF5733"></i><span>1-2</span><br>';
+        div.innerHTML +=
+          '<i style="background: #C70039"></i><span>2-3</span><br>';
+        div.innerHTML +=
+          '<i style="background: #900C3F"></i><span>3-4</span><br>';
+        div.innerHTML +=
+          '<i style="background: #581845"></i><span>4-5</span><br>';
+        div.innerHTML += '<i style="background: #DAF7A6"></i><span>5+</span>';
+
+        return div;
+      };
+      legend.addTo(myMap);
     });
-
-    // Create a baseMaps object to contain the streetmap and darkmap
-    var baseMaps = {
-      Street: streetmap,
-      Dark: darkmap,
-      Light: lightmap,
-      Satellite: satellitemap,
-    };
-
-    // Create an overlayMaps object here to contain the "State Population" and "City Population" layers
-    var overlayMaps = {
-      Heatmap: heat,
-      Markers: markers,
-    };
-
-    // Creating map object
-    var myMap = L.map("map", {
-      center: [40.73, -74.0059],
-      zoom: 2,
-      layers: [darkmap, markers],
-    });
-
-    // Create a layer control, containing our baseMaps and overlayMaps, and add them to the map
-    myMap.addLayer(markers);
-    L.control.layers(baseMaps, overlayMaps).addTo(myMap);
   });
+}
+
+function markerSize(magnitude) {
+  if (magnitude === 0) {
+    return 1;
+  }
+  return magnitude * 60000;
+}
+
+function circleColor(magnitude) {
+  switch (true) {
+    case magnitude >= 5:
+      return "#581845";
+    case magnitude >= 4:
+      return "#900C3F";
+    case magnitude >= 3:
+      return "#C70039";
+    case magnitude >= 2:
+      return "#FF5733";
+    case magnitude >= 1:
+      return "#FFC300";
+    default:
+      return "#DAF7A6";
+  }
 }
